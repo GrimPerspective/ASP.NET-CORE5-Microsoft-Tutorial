cbf66255850f42c143f42ac1555a81492cb0986a
GrimPerspective <45945593+GrimPerspective@users.noreply.github.com>
2021-04-21 01:03:42 +0800

    Add project files.

---
+++ ASP.NET(CORE5)/Data/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_CORE5_.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options) : base(options) { }
+    }
+
+}
---
+++ ASP.NET(CORE5)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_CORE5_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
