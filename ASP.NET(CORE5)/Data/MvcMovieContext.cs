95ddbd351e6e49f117054627ab4cb4eec5838ba1
GrimPerspective <45945593+GrimPerspective@users.noreply.github.com>
2021-04-21 02:09:51 +0800

    Scaffolding Added (Movies)

--- ASP.NET(CORE5)/Data/MvcMovieContext.cs
+++ ASP.NET(CORE5)/Data/MvcMovieContext.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ASP.NET_CORE5_.Models;
 
 namespace ASP.NET_CORE5_.Data
 {
     public class MvcMovieContext : DbContext
     {
         public MvcMovieContext (DbContextOptions<MvcMovieContext> options) : base(options) { }
+        public DbSet<ASP.NET_CORE5_.Models.Movie> Movie { get; set; }
     }
 
 }
